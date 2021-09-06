27abb64b7f16a2f866c6757037ff58da81e51dd9
José Pereira Macedo <arseniumgx@gmail.com>
2021-09-06 01:38:50 -0300

    Estrutura base de desenvolvimento

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {} from 'react'
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import './App.css'
+
+export default function App(){
+    return(
+        <BrowserRouter>
+            <p>teste</p>
+        </BrowserRouter>
+    )
+}
